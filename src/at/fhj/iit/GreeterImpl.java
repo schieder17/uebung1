6a87cbfa219ff359cbc290c56689dd33b09b2dc7
Markus Schieder <markus.schieder@edu.fh-joanneum.at>
2018-05-12 15:32:46 +0200

    add GreeterImpl

---
+++ src/at/fhj/iit/GreeterImpl.java
@@ -0,0 +1,21 @@
+package at.fhj.iit;
+
+public class GreeterImpl implements Greeter {
+    private String name;
+
+    public GreeterImpl(String name){
+        this.name=name;
+    }
+
+
+
+    @Override
+    public void sayHello() {
+
+    }
+
+    @Override
+    public void sayGoodbye() {
+
+    }
+}
